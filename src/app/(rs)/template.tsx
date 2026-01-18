88d3508997e55c070f204e9d3c08e42b94c5ab67
Suyash1505 <adv.mks7171@gmail.com>
2026-01-18 16:06:24 +0530

    created routes of home, customers and tickets

---
+++ src/app/(rs)/template.tsx
@@ -0,0 +1,10 @@
+
+export default function Template({ children }: 
+    Readonly<{children: React.ReactNode;}>){
+
+    return (
+        <div className="animate-appear">
+            {children}
+        </div>
+    );
+}
---
+++ src/app/(rs)/tickets/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tickets = () => {
+
+    return (
+        <div>
+            Ticket Page
+        </div>
+    )
+}
+
+export default Tickets
