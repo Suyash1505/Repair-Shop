9ab24b24b4b3a03d0fb908b23e5d849df1a8dc30
Suyash1505 <adv.mks7171@gmail.com>
2026-01-21 17:45:48 +0530

    created a database using postgresql neon

---
+++ src/db/migrations/0001_living_mister_fear.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "tickets" DROP CONSTRAINT "tickets_customerId_customers_id_fk";
+--> statement-breakpoint
+ALTER TABLE "tickets" ADD COLUMN "customer_id" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_customer_id_customers_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."customers"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tickets" DROP COLUMN "customerId";
